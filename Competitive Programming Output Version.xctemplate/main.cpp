190248ac067333ad93034c09c26b1daadf0b8e9a
Rugen Heidbuchel <rugen.heidbuchel@gmail.com>
2016-03-25 23:14:24 +0100

    Add output version

---
+++ Competitive Programming Output Version.xctemplate/main.cpp
@@ -0,0 +1,32 @@
+//
+//  ___FILENAME___
+//  ___PACKAGENAME___
+//
+//  Created by ___FULLUSERNAME___ on ___DATE___.
+//___COPYRIGHT___
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    
+    #ifdef USE_INPUT_FILE
+    freopen("input.txt", "r", stdin);
+    #endif
+    #ifdef USE_OUTPUT_FILE
+    freopen("output.txt", "w", stdout);
+    #endif
+    
+    // MAIN Begin
+    
+    <#Write your code here...#>
+    
+    // MAIN End
+    
+    #ifdef USE_OUTPUT_FILE
+    freopen("diff.txt", "w", stdout);
+    system("diff output.txt expected_output.txt");
+    #endif
+    
+    return 0;
+}
